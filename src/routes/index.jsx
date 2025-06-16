d1083de1d61d5dd41092d15396b4dc7ca6a4fc8e
hamza-mqbl <hamzaaliics@gmail.com>
2025-06-16 17:56:49 +0500

    added a route for creating account

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home.jsx";
+import Home from "../pages/HomePage";
+import SplashScreen from "../pages/SplashScreenPage";
+import CreateAccountPage from "../pages/CreateAccountStep1";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<SplashScreen />} />
+      <Route path="/create-account/step-1" element={<CreateAccountPage />} />
     </Routes>
   );
 }
