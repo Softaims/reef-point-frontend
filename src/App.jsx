20e918eec1176dff64bb7b808eaa128ee80d6889
hamza-mqbl <hamzaaliics@gmail.com>
2025-07-28 15:59:39 +0500

    remove the unecessay file import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/index.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
+import { AccountProvider } from "./contexts/AccountContext.jsx";
 
 function App() {
   return (
